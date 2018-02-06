da620a81514e241f9e19934d78589bf72e19a326
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2018-02-06 17:52:59 +0100

    2.0.0 better than ever

--- overflow-color.js
+++ overflow-color.js
@@ -5,6 +5,10 @@ var ocStyleTag;
 
 var currentColor;
 
+var bodyBackground;
+
+var bodyWrap;
+
 function setBgColor(color) {
 
     currentColor = color;
@@ -52,17 +56,32 @@ function handleScroll() {
 
 function initOverflowColor() {
 
-    var topEl,
-        bottomEl;
+    var topEl = document.querySelector('[data-oc-top]'),
+        bottomEl = document.querySelector('[data-oc-bottom]');
 
-    if (topEl = document.querySelector('[data-oc-top]')) {
+    if (topEl) {
         ocTopColor = topEl.getAttribute('data-oc-top');
     }
-    if (bottomEl = document.querySelector('[data-oc-bottom]');) {
+    if (bottomEl) {
         ocBottomColor = bottomEl.getAttribute('data-oc-bottom');
     }
 
     if (ocTopColor || ocBottomColor) {
+
+        var bodyComputedStyle = window.getComputedStyle(document.body, null);
+        bodyBackground = bodyComputedStyle.getPropertyValue('background');
+        if (bodyComputedStyle.getPropertyValue('background-color') === 'rgba(0, 0, 0, 0)' && bodyBackground.substring(21, 17) === 'none') {
+            bodyBackground = 'white';
+        }
+        document.body.style.background = 'transparent';
+        bodyWrap = document.createElement('div');
+        bodyWrap.setAttribute('data-oc-wrap', '');
+        bodyWrap.style.background = bodyBackground;
+        for (var i = 0, l = document.body.childNodes.length; i < l; i++) {
+            bodyWrap.appendChild(document.body.childNodes[0]);
+        }
+        document.body.appendChild(bodyWrap);
+
         if (!ocTopColor && ocBottomColor) {
             ocTopColor = ocBottomColor;
         }
@@ -85,13 +104,19 @@ function initOverflowColor() {
             window.attachEvent('scroll', handleScroll);
             window.attachEvent('resize', handleScroll);
         }
+
     }
 
 }
 
-if (typeof document.addEventListener !== 'undefined') {
-    document.addEventListener('DOMContentLoaded', initOverflowColor, false);
+if (document.readyState === "complete" || document.readyState === "loaded") {
+    initOverflowColor();
 }
 else {
-    document.attachEvent('onreadystatechange', initOverflowColor);
+    if (typeof document.addEventListener !== 'undefined') {
+        document.addEventListener('DOMContentLoaded', initOverflowColor, false);
+    }
+    else {
+        document.attachEvent('onreadystatechange', initOverflowColor);
+    }
 }
--- readme.md
+++ readme.md
@@ -4,6 +4,11 @@ Javascript script that automatically switch css html background color on smartph
 
 ![Demo](gif.gif)
 
+This package is on npm
+```console
+$ npm install --save overflow-color
+```
+
 ## Usage
 
 You simply must add the script and two attributes to your body tag
@@ -11,6 +16,39 @@ You simply must add the script and two attributes to your body tag
 ```html
 <body data-oc-top="red" data-oc-bottom="blue">
     <!-- ... -->
-    <script src="overflow-color.js"></script>
+    <script src="overflow-color.min.js"></script>
 </body>
 ```
+
+## CSS tricks
+#### Behind the Scenes
+
+This library will wrap all the body content inside a `<div data-oc-wrap>`.
+Then it set to the wrapper the same background as the body, and set body's background to `transparent`.
+
+when the document is loaded:
+```html
+<head>
+    <!-- ... -->
+    <style>
+        body {
+            background: lightgrey;
+        }
+    </style>
+</head>
+<body data-oc-top="red" data-oc-bottom="blue" style="background: transparent;">
+    <div data-oc-wrap style="background: lightgrey;">
+        <!-- ... -->
+        <script src="overflow-color.min.js"></script>
+    </div>
+</body>
+```
+
+## Build
+
+Minify script
+```console
+$ npm run build
+```
+
+## [License](LICENSE.txt)
